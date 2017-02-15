a10f8db9761813f764a2967c9b23c26c7af9e1b4
Student <Student@SDEH131-19C4DL>
2017-02-15 13:19:48 -0600

    First commit

--- cn2/cn2.cpp
+++ cn2/cn2.cpp
@@ -2,10 +2,65 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+unsigned short chkSum(unsigned short * buf, int count);
+
+using namespace std;
 
 int main()
 {
+	int guess;
+	char  a  [] = "No winter lasts forever; no spring skips its turn.\0";
+	srand(time(NULL));
+	unsigned short b[sizeof(a)/sizeof(a[0])];
+	unsigned short ret;
+
+	cout << a.size() << endl;
+	cout << b.size() << endl;
+
+	//Compress data, 2 char for each unsigned short
+	for (int i = 0; i < strlen(a); i+=2)
+	{
+		b[i] = a[i] + a[i+1];
+
+		cout << b[i] << endl;
+	}
+
+
+
+	//Message without altering
+	/*ret = chkSum(b, sizeof(b));
+	cout << ret << endl;
+
+	//Alter one bit, randomly
+
+	cout << sizeof(b) << endl;
+	guess = rand() % sizeof(b) +1;
+	cout << guess << endl;
+	*/
+
     return 0;
+}
+
+unsigned short chkSum(unsigned short * buf, int count )
+{
+	register unsigned long sum = 0;
+
+	while (count--)
+	{
+		sum += *buf++;
+		if (sum & 0xFFFF0000)
+		{
+			//Carry occured
+			sum &= 0xFFFF;
+			sum++;
+		}
+
+	}
+	return ~(sum & 0xFFFF);
+
 }
 
