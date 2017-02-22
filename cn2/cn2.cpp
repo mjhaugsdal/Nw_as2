6cedd64fb2bff6c4e8931c272c8abb9094f7a6fd
Student <Student@SDEH131-03C4DL>
2017-02-22 15:50:19 -0600

    Maybe done?

--- cn2/cn2.cpp
+++ cn2/cn2.cpp
@@ -8,74 +8,135 @@
 #include <bitset>
 #include <vector>
 
+using namespace std;
+
 unsigned short chkSum(unsigned short * buf, int count);
+vector<unsigned short> compress(char * a);
+char * msgErr(char * a, int b, int c);
+
 
 using namespace std;
 
 int main()
 {
 	int guess;
-	char  a  [] = "No winter lasts forever; no spring skips its turn.\0";
+	char  arr  [] = "No winter lasts forever; no spring skips its turn.";
 
 	srand(time(NULL));
 
-	//unsigned short b[];
 	vector<unsigned short> b;
+
 	unsigned short ret;
+
 
 	//Compress data, 2 char for each unsigned short
 
 
-	for (int i = 0; i < strlen(a); i+=2)
-	{
-		for (int j = i; j <= i; j++)
-		{
-			b.push_back(a[j] + a[j + 1]);	
-		}
-		
 
 
-	}
+	/*for (int i = 0; i < b.size() ; i++)
+		cout << b[i] << endl;*/
 
-	for (int i = 0; i < b.size(); i++)
-		cout << b[i] << endl;
+	cout << "This program will take the following message: ";
+	cout << arr << endl;
+	cout << "And do the following operations: " << endl;
+	
+	cout << "1. Compress 2 and 2 characters to single unsigned shorts" << endl;
+	cout << "2. Perform a checksum algorithm on the message and show both message and checksum to the screen." << endl;
+	cout << "3. Randomly permute the message, and show 1, 3 and 8 bit errors to the screen" << endl;
+	cout << endl;
+
+
 
 	//Message without altering
-
+	b = compress(arr);
 	unsigned short * d = &b[0];
 	ret = chkSum(d, sizeof(b)/  sizeof(b[0]));
+	cout << "Checksum of original message:  " << ret << endl;
+
+
+	//cout << guess << endl;
 
 
 	//Pick a number randomly
 	guess = rand() % sizeof(b) / sizeof(b[0]) + 1;
-	//cout << guess << endl;
-
-
 	//Alter one bit, randomly
-	bitset <8> set(a[guess]);
-	set.flip(0);
-	unsigned long i = set.to_ulong();
-	unsigned char c = static_cast<unsigned char>(i); // simplest -- no checks for 8 bit bitsets
-	a[guess] = c;
-	cout << a << endl;
 
+	strcpy_s(arr, msgErr(arr, 1, guess));
+	cout << arr << endl;
+	b = compress(arr);
+	d = &b[0];
+	ret = chkSum(d, sizeof(b) / sizeof(b[0]));
+	cout << "Checksum: " << ret << endl;
 
-	//Pick new number randomly
+	//Pick a number randomly
 	guess = rand() % sizeof(b) / sizeof(b[0]) + 1;
-	//Alter 3 bits, randomly
-	bitset <8 >set2(a[guess]);
-	set2.flip(0);
-	set2.flip(1);
-	set2.flip(2);
-	i = set2.to_ulong();
-	c = static_cast<unsigned char>(i); // simplest -- no checks for 8 bit bitsets
-	a[guess] = c;
-	cout << a;
+	//3 bits
+	strcpy_s(arr, msgErr(arr, 3, guess));
+	cout << arr << endl;
+	b = compress(arr);
+	d = &b[0];
+	ret = chkSum(d, sizeof(b) / sizeof(b[0]));
+	cout << "Checksum: " << ret << endl;
+
+
+	//Pick a number randomly
+	guess = rand() % sizeof(b) / sizeof(b[0]) + 1;
+	//8 bits
+	strcpy_s(arr, msgErr(arr, 8, guess));
+	cout << arr << endl;
+	b = compress(arr);
+	d = &b[0];
+	ret = chkSum(d, sizeof(b) / sizeof(b[0]));
+	cout << "Checksum: " << ret << endl;
+
 
 
 
     return 0;
 }
+
+char * msgErr(char * arr, int bit, int guess)
+{
+
+	//Alter one bit, randomly
+	bitset <8> set(arr[guess]);
+
+	for (int i = 0; i < bit; i++)
+	{
+		set.flip(i);
+	}
+
+	unsigned long i = set.to_ulong();
+	unsigned char c = static_cast<unsigned char>(i);
+	arr[guess] = c;
+
+	return arr;
+
+}
+
+
+vector<unsigned short> compress(char * a)
+{
+
+	vector<unsigned short> b;
+
+	for (int i = 0; i < strlen(a) / 2; i++)
+	{
+		unsigned short t1, t2;
+		t1 = a[i + i];
+		t2 = a[i + i + 1];
+		//Shift
+		t1 << 8;
+		t1 |= t2;
+
+		b.push_back(t1);
+
+	}
+
+	return b;
+}
+
 
 unsigned short chkSum(unsigned short * buf, int count )
 {
